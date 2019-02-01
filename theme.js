bf0acd23e1b9ebbc3269470fec6650afa115ce1a
Matt Williams <mwilliams@change.org>
2019-01-31 17:51:39 -0800

    WIP

---
+++ theme.js
@@ -0,0 +1,16 @@
+import theme from "mdx-deck/themes";
+
+export default {
+  // extends the default theme
+  ...theme,
+  // add a custom font
+  // font: "Change Calibre, Helvetica Neue, Helvetica, Arial, Tahoma, sans-serif",
+  font: "Change Calibre",
+
+  // custom colors
+  colors: {
+    text: "#fff",
+    background: "#ec2c22",
+    link: "#0ff"
+  }
+};
